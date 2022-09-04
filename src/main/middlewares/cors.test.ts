8c32c3eab41f1a09d6171c14657b649a84d3876e
Gabriel <gabrielturing@gmail.com>
2022-09-04 18:16:41 -0300

    refactor: remove req.body to test cors

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -4,7 +4,7 @@ import app from '../config/app'
 describe('Cors Middleware', () => {
   test('Should enable CORS', async () => {
     app.get('/test_cors', (req, res) => {
-      res.send(req.body)
+      res.send()
     })
 
     await request(app)
